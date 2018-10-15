a5502427ed6865a35953b46c08185b942d7bfdf9
Milly Rodriguez <millz118@gmail.com>
2018-10-15 17:24:30 -0400

    completed more on react crash course including states and lifting of properties, navbar

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.value
-    };
-
-    handleIncrement = product => {
-        this.setState({ value: this.state.value + 1 });
-    };
-
     render() {
         console.log(this.props);
 
@@ -18,26 +10,29 @@ class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button
-                    onClick={() => this.handleIncrement({ id: 1 })}
+                    onClick={() => this.props.onIncrement(this.props.counter)}
                     className="btn btn-secondary btn-sm m-2"
                 >
                     +
                 </button>
-                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm">
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm"
+                >
                     -
                 </button>
-            </div >
+            </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? "Zero" : value;
     }
 }
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,10 +1,11 @@
+// left of 2:06 https://www.youtube.com/watch?v=Ke90Tje7VS0
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
