c5e2c24d3d0587153f80e0ea7f7668abc81a474d
Milly Rodriguez <millz118@gmail.com>
2018-10-16 16:22:10 -0400

    stateless functional components

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar
-                    <span className="badge badge-pill badge-secondary">
-                        {this.props.totalCounters}
-                    </span>
-                </a>
-            </nav>
-        );
-    }
-}
+//stateless functional component - since this class has a single method, no event handler or helper methods to calculate values and no state either, in this situation we can convert this to a stateless functional component. In functional components you have to manually pass in the props parameter
+const NavBar = props => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+                Navbar{" "}
+                <span className="badge badge-pill badge-secondary">
+                    {props.totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
+};
 
 export default NavBar;
